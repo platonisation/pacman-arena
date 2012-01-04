bb5b0ad3081ba78e79fda4d4ccd3ffacd29a0cfd
timote.bonnin@gmail.com <timote.bonnin@gmail.com@76d508e0-3886-af98-f917-87a69587fad2>
2012-01-04 17:43:26 +0000

    indentation des fichers

--- trunk/code/main.cpp
+++ trunk/code/main.cpp
@@ -7,38 +7,38 @@ using namespace sf;
 
 int main ( int argc, char *argv[] )
 {
-	
-	// Ouverture de la fenêtre avec la SMFL
-	RenderWindow window ( VideoMode ( 800, 600, 32 ), "Ma premiere fenetre SFML ! " ) ;
-	
-	// Déclaration des variables
-	int action = 1 ;
-	//myOption opt = myOption ( ) ;
-	
-	while ( action != 0 )
-	{
-		
-		switch ( action )
-		{
-		
-			case 1 :
-				// Menu
-				action = menu ( window, opt ) ;
-				break ;
-				
-			default :
-				action = 0 ;
-				break ;
-				
-		}
-		
-	}
-	
-	// Libération de la mémoire
-	// ...
-	
-	// Fermeture de la SDL
-	window.Close ( ) ;
-	
-	return EXIT_SUCCESS;
+
+    // Ouverture de la fenêtre avec la SMFL
+    RenderWindow window ( VideoMode ( 800, 600, 32 ), "Ma premiere fenetre SFML ! " ) ;
+
+    // Déclaration des variables
+    int action = 1 ;
+    //myOption opt = myOption ( ) ;
+
+    while ( action != 0 )
+    {
+
+        switch ( action )
+        {
+
+        case 1 :
+            // Menu
+            action = menu ( window, opt ) ;
+            break ;
+
+        default :
+            action = 0 ;
+            break ;
+
+        }
+
+    }
+
+    // Libération de la mémoire
+    // ...
+
+    // Fermeture de la SDL
+    window.Close ( ) ;
+
+    return EXIT_SUCCESS;
 }
