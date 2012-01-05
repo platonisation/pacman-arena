0c60aaa06d033b218a426459bb9b128210ce82ad
roadkiller.cl@gmail.com <roadkiller.cl@gmail.com@76d508e0-3886-af98-f917-87a69587fad2>
2012-01-05 11:41:53 +0000

    classe myOption fonctionnelle, reste à lui faire la sauvegarde/chargement

--- trunk/code/main.cpp
+++ trunk/code/main.cpp
@@ -3,42 +3,40 @@
 #include "myOption.h"
 #include "action.h"
 
-using namespace sf;
+using namespace sf ;
 
 int main ( int argc, char *argv[] )
 {
-
+	
     // Ouverture de la fenêtre avec la SMFL
-    RenderWindow window ( VideoMode ( 800, 600, 32 ), "Ma premiere fenetre SFML ! " ) ;
-
+    RenderWindow window ( VideoMode ( 800, 600, 32 ), "Pacman Arena" ) ;
+	
     // Déclaration des variables
     int action = 1 ;
-    //myOption opt = myOption ( ) ;
-
+    myOption opt = myOption ( ) ;
+	
     while ( action != 0 )
     {
-
+		
         switch ( action )
         {
-
+			
         case 1 :
             // Menu
             action = menu ( window, opt ) ;
             break ;
-
+			
         default :
             action = 0 ;
             break ;
-
+			
         }
-
+		
     }
-
-    // Libération de la mémoire
-    // ...
-
-    // Fermeture de la SDL
+	
+    // Fermeture de la fenêtre SFML
     window.Close ( ) ;
-
-    return EXIT_SUCCESS;
+	
+    return EXIT_SUCCESS ;
+    
 }
