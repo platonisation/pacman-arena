de809fc2a56e6b9bc15d1a2fb8db1eaa40f80134
roadkiller.cl@gmail.com <roadkiller.cl@gmail.com@76d508e0-3886-af98-f917-87a69587fad2>
2012-01-05 13:35:58 +0000

    Ajout d'un paramètre de type verbose/DEBUG et main.cpp terminé à moins de rajouter d'autres paramètres

--- code/action.h
+++ code/action.h
@@ -2,12 +2,13 @@
 
 #define __ACTION_H__
 #include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
 #include "myOption.h"
 
 int menu ( sf::RenderWindow& window, myOption& opt ) ;
 int option ( sf::RenderWindow& window, myOption& opt ) ;
-int create ( sf::RenderWindow& window, myOption& opt ) ; // Socket à rajouter
-int join ( sf::RenderWindow& window, myOption& opt ) ; // Socket à rajouter
-int play ( sf::RenderWindow& window, myOption& opt ) ; // Socket à rajouter
+int create ( sf::RenderWindow& window, myOption& opt, sf::SocketTCP& sck ) ;
+int join ( sf::RenderWindow& window, myOption& opt, sf::SocketTCP& sck ) ;
+int play ( sf::RenderWindow& window, myOption& opt, sf::SocketTCP& sck ) ;
 
 #endif
