a8c309eedf532b6036234adccb83b6996cbea2d3
roadkiller.cl@gmail.com <roadkiller.cl@gmail.com@76d508e0-3886-af98-f917-87a69587fad2>
2012-01-05 11:41:53 +0000

    classe myOption fonctionnelle, reste à lui faire la sauvegarde/chargement

--- code/myOption.cpp
+++ code/myOption.cpp
@@ -2,19 +2,30 @@
 
 using namespace sf ;
 
-myOption::myOption ( ) { }
+myOption::myOption ( )
+{
+	
+	this->up = Key::Up ;
+	this->down = Key::Down ;
+	this->left = Key::Left ;
+	this->right = Key::Right ;
+	this->valid = Key::Return ;
+	this->cancel = Key::Escape ;
+	
+}
+
 myOption::~myOption ( ) { }
 
-Key::Code myOption::getUpKey ( ) { }
-Key::Code myOption::getDownKey ( ) { }
-Key::Code myOption::getLeftKey ( ) { }
-Key::Code myOption::getRightKey ( ) { }
-Key::Code myOption::getValidKey ( ) { }
-Key::Code myOption::getCancelKey ( ) { }
+Key::Code myOption::getUpKey ( ) { return this->up ; }
+Key::Code myOption::getDownKey ( ) { return this->down ; }
+Key::Code myOption::getLeftKey ( ) { return this->left ; }
+Key::Code myOption::getRightKey ( ) { return this->right ; }
+Key::Code myOption::getValidKey ( ) { return this->valid ; }
+Key::Code myOption::getCancelKey ( ) { return this->cancel ; }
 
-void myOption::setUpKey ( Key::Code key ) { }
-void myOption::setDownKey ( Key::Code key ) { }
-void myOption::setLeftKey ( Key::Code key ) { }
-void myOption::setRightKey ( Key::Code key ) { }
-void myOption::setValidKey ( Key::Code key ) { }
-void myOption::setCancelKey ( Key::Code key ) { }
+void myOption::setUpKey ( Key::Code key ) { this->up = key ; }
+void myOption::setDownKey ( Key::Code key ) { this->down = key ; }
+void myOption::setLeftKey ( Key::Code key ) { this->left = key ; }
+void myOption::setRightKey ( Key::Code key ) { this->right = key ; }
+void myOption::setValidKey ( Key::Code key ) { this->valid = key ; }
+void myOption::setCancelKey ( Key::Code key ) { this->cancel = key ; }
