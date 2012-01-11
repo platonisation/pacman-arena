7f59ff55452880320897afad3aaf17e3765cd65d
timote.bonnin@gmail.com <timote.bonnin@gmail.com@76d508e0-3886-af98-f917-87a69587fad2>
2012-01-11 07:44:09 +0000

    fin option

--- code/option.cpp
+++ code/option.cpp
@@ -18,6 +18,7 @@ int option ( RenderWindow& window, myOption& opt )
 		s_haut = String ( "Haut" ),
 		s_bas = String ( "Bas" ) ,
 		s_droit = String ( "Droite" ),
+		s_quitter = String ( "Quitter" ),
 		//touches modifiables
 		s_gaucheM = String ( ),
 		s_hautM = String ( ),
@@ -46,6 +47,7 @@ int option ( RenderWindow& window, myOption& opt )
 	s_gauche.SetPosition ( 70, window.GetHeight ( ) - 200 ) ;
 	s_bas.SetPosition ( 70, window.GetHeight ( ) - 160 ) ;
 	s_droit.SetPosition ( 70, window.GetHeight ( ) - 120 ) ;
+	s_quitter.SetPosition ( 70, window.GetHeight ( ) - 50 ) ;
 	//touches modifiables
 	s_hautM.SetPosition ( 250, window.GetHeight ( ) - 240 ) ;
 	s_gaucheM.SetPosition ( 250, window.GetHeight ( ) - 200 ) ;
@@ -64,12 +66,13 @@ int option ( RenderWindow& window, myOption& opt )
 		s_haut.SetColor ( Color ( 255, 255, 255, ( action == 3 ) ? 255 : 150 ) ) ;
 		s_gauche.SetColor ( Color ( 255, 255, 255, ( action == 2 ) ? 255 : 150 ) ) ;
 		s_bas.SetColor ( Color ( 255, 255, 255, ( action == 0 ) ? 255 : 150 ) ) ;
-		s_droit.SetColor ( Color ( 255, 255, 255, ( action == 4 ) ? 255 : 150 ) ) ;
+		s_droit.SetColor ( Color ( 255, 255, 255, ( action == 5 ) ? 255 : 150 ) ) ;
+		s_quitter.SetColor ( Color ( 255, 255, 255, ( action == 4 ) ? 255 : 150 ) ) ;
 		//Touches modifiables
 		s_hautM.SetColor ( Color ( 255, 255, 255, ( action == 3 ) ? 255 : 150 ) ) ;
 		s_gaucheM.SetColor ( Color ( 255, 255, 255, ( action == 2 ) ? 255 : 150 ) ) ;
 		s_basM.SetColor ( Color ( 255, 255, 255, ( action == 0 ) ? 255 : 150 ) ) ;
-		s_droitM.SetColor ( Color ( 255, 255, 255, ( action == 4 ) ? 255 : 150 ) ) ;
+		s_droitM.SetColor ( Color ( 255, 255, 255, ( action == 5 ) ? 255 : 150 ) ) ;
 		
 		window.Draw ( s_touches ) ;
 		window.Draw ( s_haut ) ;
@@ -80,6 +83,7 @@ int option ( RenderWindow& window, myOption& opt )
 		window.Draw ( s_gaucheM ) ;
 		window.Draw ( s_basM ) ;
 		window.Draw ( s_droitM	 ) ;
+		window.Draw ( s_quitter ) ;
 		window.Draw ( Rect ) ;
 		
 		window.Display ( ) ;
@@ -101,7 +105,7 @@ int option ( RenderWindow& window, myOption& opt )
 				{
 					if ( action == 0 )
 						action = 2 ;
-					else if ( action == 4 )
+					else if ( action == 5 )
 						action = 0 ;
 					else
 						action ++ ;
@@ -109,7 +113,7 @@ int option ( RenderWindow& window, myOption& opt )
 				else if ( event.Key.Code == opt.getDownKey() ) // Appui sur la touche Bas
 				{
 					if ( action == 0 )
-						action = 4 ;
+						action = 5 ;
 					else if ( action == 2 )
 						action = 0 ;
 					else
@@ -117,44 +121,59 @@ int option ( RenderWindow& window, myOption& opt )
 				}
 				else if ( event.Key.Code == opt.getValidKey() ) // Appui sur la touche Valider
 				{
-					working = !working;
-					Event event2;
-					//boucle de choix de touche
-					while ( working )
+					if ( action == 4 )
 					{
-						while ( window.GetEvent( event2 ) )
-						{
-						if ( event2.Type == Event::KeyPressed )
-						{
-							if ( action == 4 )
-							{
-								opt.setRightKey( event2.Key.Code ) ;
-								droit = static_cast<char>( event2.Key.Code );
-								s_droitM.SetText( droit ) ;
-							}
-							else if ( action == 2 )
-							{
-								opt.setLeftKey( event2.Key.Code ) ;
-								gauche = static_cast<char>(event2.Key.Code);
-								s_gaucheM.SetText( gauche ) ;
-							}
-							else if ( action == 3 )
-							{
-								opt.setUpKey( event2.Key.Code ) ;
-								haut = static_cast<char>( event2.Key.Code );
-								s_hautM.SetText( haut ) ;
-							}
-							else
-							{
-								opt.setDownKey ( event2.Key.Code ) ;
-								bas = static_cast<char>( event2.Key.Code );
-								s_basM.SetText( bas ) ;
-							}
-							working = !working;
-						}
+						quitter = true ;
+						action = 1 ;
 					}
-				}
-				}
+					else
+					{
+						working = !working;
+						Event event2;
+						//boucle de choix de touche
+						while ( working )
+						{
+							while ( window.GetEvent( event2 ) )
+							{
+								if ( event2.Type == Event::KeyPressed )
+								{
+									if ( action == 5 )
+									{
+										opt.setRightKey( event2.Key.Code ) ;
+										droit = static_cast<char>( event2.Key.Code );
+										s_droitM.SetText( droit ) ;
+									}
+									else if ( action == 2 )
+									{
+										opt.setLeftKey( event2.Key.Code ) ;
+										gauche = static_cast<char>(event2.Key.Code);
+										s_gaucheM.SetText( gauche ) ;
+									}
+									else if ( action == 3 )
+									{
+										opt.setUpKey( event2.Key.Code ) ;
+										haut = static_cast<char>( event2.Key.Code );
+										s_hautM.SetText( haut ) ;
+									}
+									else if ( action == 0 )
+									{
+										opt.setDownKey ( event2.Key.Code ) ;
+										bas = static_cast<char>( event2.Key.Code );
+										s_basM.SetText( bas ) ;
+									}
+									
+									working = !working ;
+									
+								} // fin condition key pressed
+								
+							} // fin detection evenement
+							
+						} // fin boucle d'attente d'une touche pressé
+						
+					} // fin condition modification touche
+					
+				} // fin appuie touche return
+				
 			break;
 			
 			default :
